283ecca97b30af276c468f9b797ce7bf27016b5a
Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>
2026-01-07 03:10:51 -0500

    Refactored Update Required logic to address Feature Request #55
    Signed-off-by: Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>

--- Resources/JamfEA-DDM_Executed_OS_Update_Date.zsh
+++ Resources/JamfEA-DDM_Executed_OS_Update_Date.zsh
@@ -3,7 +3,7 @@
 # EA: DDM Executed OS Update Date (log-based)
 # Reports when the CURRENT OS version/build first appeared in /var/log/install.log.
 #
-# Version: 2.3.0b1
+# Version: 2.3.0b2
 # Date: 07-Jan-2026
 
 # Safety: don't use -e or pipefail in Jamf EA context

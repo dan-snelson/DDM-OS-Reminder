3350b83582a54a8368d0c2b422be43e249d91552
Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>
2026-03-31 19:25:13 -0400

    3.1.0b9
    Updated artifacts
    
    Signed-off-by: Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>

--- launchDaemonManagement.zsh
+++ launchDaemonManagement.zsh
@@ -30,7 +30,7 @@
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local:/usr/local/bin
 
 # Script Version
-scriptVersion="3.1.0b8"
+scriptVersion="3.1.0b9"
 
 # Client-side Log
 scriptLog="/var/log/org.churchofjesuschrist.log"

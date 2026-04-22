022f7ca5e10a35f14e832b10456ffb2568f9a412
Dan K. Snelson <24623109+dan-snelson@users.noreply.github.com>
2026-04-22 13:47:30 -0400

    3.2.0b2

--- launchDaemonManagement.zsh
+++ launchDaemonManagement.zsh
@@ -30,7 +30,7 @@
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local:/usr/local/bin
 
 # Script Version
-scriptVersion="3.1.0"
+scriptVersion="3.2.0b2"
 
 # Client-side Log
 scriptLog="/var/log/org.churchofjesuschrist.log"
